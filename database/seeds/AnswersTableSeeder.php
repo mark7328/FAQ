050074e29350834d3f35c4684515422d72d049fa
Martin Chukwu <mark7328chukwu@gmail.com>
2019-05-12 14:28:34 -0400

    Added various views and controllers updated answers seed updated web route

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -12,6 +12,7 @@ class AnswersTableSeeder extends Seeder
     public function run()
     {
         $users = App\User::inRandomOrder();
+        for ($i = 1; $i <= 6; $i++) {
             $users->each(function ($user) {
                 $question = App\Question::inRandomOrder()->first();
                 $answer = factory(\App\Answer::class)->make();
@@ -20,4 +21,5 @@ class AnswersTableSeeder extends Seeder
                 $answer->save();
             });
         }
+    }
 }
