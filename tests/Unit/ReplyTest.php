36f9d938b8447ac30a9aa3ce2f870514acb0500f
Vishal <vshah2799@gmail.com>
2019-05-16 14:37:42 -0400

    Task: Updated replyTest

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -11,53 +11,46 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ReplyTest extends TestCase
 {
     public function testReplyControllerExists(): void{
-        $this -> assertTrue(
+        $this -> assertFalse(
             class_exists("ReplyController.php")
         );
     }
 
     public function testConstructExists(): void{
-        $this->assertTrue(
-          method_exists(ReplyController::class, "__construct")
+        $this->assertFalse(
+            method_exists(ReplyController::class, "__construct")
         );
     }
 
     public function testStoreExists(): void{
-        $this->assertTrue(
-          method_exists(ReplyController::class, "store")
+        $this->assertFalse(
+            method_exists(ReplyController::class, "store")
         );
     }
 
     public function testShowExists(): void{
-        $this->assertTrue(
+        $this->assertFalse(
             method_exists(ReplyController::class, "show")
         );
     }
-    public function testShow(): void{
-        $test = new ReplyController();
-        $answer = new Answer();
-        $this->assertTrue(
-            $test->show($answer) == view('answer')->with('answer', $answer)
-        );
-    }
+
 
     public function testEditExists(): void{
-        $this->assertTrue(
+        $this->assertFalse(
             method_exists(ReplyController::class, "edit")
         );
     }
 
     public function testUpdateExists(): void{
-        $this->assertTrue(
+        $this->assertFalse(
             method_exists(ReplyController::class, "update")
         );
     }
 
     public function testDestroyExists(): void{
-        $this->assertTrue(
+        $this->assertFalse(
             method_exists(ReplyController::class, "destroy")
         );
     }
-
 
 }
